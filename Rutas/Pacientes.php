bcf3dc5b5ed868e9a0782886406f6fff98ab7581
Hugo Munoz <proyectos.munoz17@gmail.com>
2025-02-25 12:26:17 -0700

    Guardar citas programadas y carpeta de funciones

--- Rutas/Pacientes.php
+++ Rutas/Pacientes.php
@@ -59,8 +59,9 @@ return function (Micro $app,$di) {
 
             if (!empty($id_servicio)){
                 $phql   .= " AND EXISTS (
-                                SELECT 1 FROM ctpacientes_servicios t1
-                                WHERE t1.id_servicio = :id_servicio AND a.id = t1.id_profesional
+                                SELECT 1 FROM tbcitas_programadas t1
+                                LEFT JOIN tbcitas_programadas_servicios t2 ON t1.id = t2.id_cita_programada
+                                WHERE t2.id_servicio = :id_servicio AND a.id = t1.id_paciente
                             )";
 
                 $values['id_servicio']  = $id_servicio;
@@ -120,7 +121,9 @@ return function (Micro $app,$di) {
                         FROM ctpacientes a 
                         LEFT JOIN (
                             SELECT t1.id_paciente, COUNT(1) as num_servicios
-                            FROM ctpacientes_servicios t1
+                            FROM tbcitas_programadas t1
+                            LEFT JOIN tbcitas_programadas_servicios t2 ON t1.id = t2.id_cita_programada
+                            WHERE t2.id IS NOT NULL
                             GROUP BY t1.id_paciente
                         ) b ON a.id = b.id_paciente
                         LEFT JOIN ctlocaciones c ON a.id_locacion_registro = c.id
@@ -154,12 +157,14 @@ return function (Micro $app,$di) {
 
             if (!empty($id_servicio)){
                 $phql   .= " AND EXISTS (
-                                SELECT 1 FROM ctpacientes_servicios t1
-                                WHERE t1.id_servicio = :id_servicio AND a.id = t1.id_profesional
+                                SELECT 1 FROM tbcitas_programadas t1
+                                LEFT JOIN tbcitas_programadas_servicios t2 ON t1.id = t2.id_cita_programada
+                                WHERE t2.id_servicio = :id_servicio AND a.id = t1.id_paciente
                             )";
 
                 $values['id_servicio']  = $id_servicio;
             }
+
 
             if (is_numeric($id_locacion_registro)){
                 $phql           .= " AND a.id_locacion_registro = :id_locacion_registro";
@@ -296,5 +301,233 @@ return function (Micro $app,$di) {
                 'message' => $e->getMessage()
             ])->setStatusCode(400, 'Bad Request');
         }
+    });
+
+    $app->post('/ctpacientes/save_program_date', function () use ($app, $db, $request) {
+        $conexion = $db; 
+        try {
+            $conexion->begin();
+    
+            // OBTENER DATOS JSON
+            $id_paciente    = $request->getPost('id_paciente') ?? null;
+            $id_locacion    = $request->getPost('id_locacion') ?? null;
+            $obj_info       = $request->getPost('obj_info') ?? null;
+            $accion         = 'pendiete de realizar';
+
+            $arr_dias   = array(
+                1   => 'Lunes',
+                2   => 'Martes',
+                3   => 'Miercoles',
+                4   => 'Jueves',
+                5   => 'Viernes',
+                6   => 'Sabado',
+                7   => 'Domingo'
+            );
+            
+            // VERIFICAR QUE CLAVE Y NOMBRE NO ESTEN VACÍOS
+            if (empty($id_paciente)) {
+                throw new Exception('Parámetro "Paciente" vacío');
+            }
+
+            if (empty($id_locacion)){
+                throw new Exception('Parámetro "Locación" vacío');
+            }
+
+            // if (empty($obj_info)) {
+            //     throw new Exception('Lista de servicios vacia');
+            // }
+
+            //  SE BUSCA ID DE TBCITAS_PROGRAMADAS, DE NO EXISTIR ESTA SE CREA
+            $phql   = " SELECT * FROM tbcitas_programadas 
+                        WHERE id_paciente = :id_paciente AND id_locacion = :id_locacion";
+            $values = array(
+                'id_paciente'   => $id_paciente,
+                'id_locacion'   => $id_locacion
+            );
+
+            $result = $db->query($phql, $values);
+            $result->setFetchMode(\Phalcon\Db\Enum::FETCH_ASSOC);
+    
+            $id_cita_programada = null;
+            if ($result) {
+                while ($data = $result->fetch()) {
+                    $id_cita_programada = $data['id'];
+
+                    //  SE BORRAN LOS REGISTROS DE SERVICIOS Y HORARIOS
+                    $phql   = "DELETE FROM tbcitas_programadas_servicios WHERE id_cita_programada = :id_cita_programada";
+                    $result_delete  = $conexion->execute($phql,array('id_cita_programada'   => $id_cita_programada));
+                }
+            }
+
+            if ($id_cita_programada == null){
+
+                $phql   = "INSERT INTO tbcitas_programadas (id_paciente,id_locacion) 
+                            VALUES (:id_paciente,:id_locacion) RETURNING *";
+                $values = array(
+                    'id_paciente'   => $id_paciente,
+                    'id_locacion'   => $id_locacion
+                );
+
+                $result = $conexion->query($phql, $values);
+                $result->setFetchMode(\Phalcon\Db\Enum::FETCH_ASSOC);
+        
+                $id_cita_programada = null;
+                if ($result) {
+                    while ($data = $result->fetch()) {
+                        $id_cita_programada = $data['id'];
+                    }
+                }
+            }
+    
+            //  SE RECORRE EL OBJ CON LAS NUEVAS CITAS
+            foreach($obj_info as $info_cita){
+
+                $id_profesional = $info_cita['id_profesional'];
+                $id_servicio    = $info_cita['id_servicio'];
+                $id_cita_programada_servicio    = null;
+
+                //  SE CREA EL REGISTRO DE TBCITAS_PROGRAMADAS_SERVICIOS
+                $phql   = "INSERT INTO tbcitas_programadas_servicios (id_cita_programada,id_servicio,id_profesional)
+                            VALUES (:id_cita_programada,:id_servicio,:id_profesional) RETURNING *";
+
+                $result_servicios   = $conexion->query($phql, array(
+                    'id_cita_programada'    => $id_cita_programada,
+                    'id_servicio'           => $id_servicio,
+                    'id_profesional'        => $id_profesional
+                ));
+                $result_servicios->setFetchMode(\Phalcon\Db\Enum::FETCH_ASSOC);
+
+                if ($result_servicios) {
+                    while ($data_servicios = $result_servicios->fetch()) {
+                        $id_cita_programada_servicio    = $data_servicios['id'];
+                    }
+                }
+
+                //  SE RECORRE LA ESTRUCTURA DE LAS HORAS DE LAS CITAS
+                foreach($info_cita['horarios'] as $horario){
+                    //  SE EJECUTA FUNCION PARA VALIDAR EMPALMADOS
+                    try{
+                        $phql   = "SELECT * FROM fn_validar_citas_programadas(:id_profesional, :id_paciente, :dia,:label_dia, :hora_inicio, :hora_termino)";
+                        $values = array(
+                            'id_profesional'    => $id_profesional,
+                            'id_paciente'       => $id_paciente,
+                            'dia'               => $horario['dia'],
+                            'label_dia'         => $arr_dias[$horario['dia']],
+                            'hora_inicio'       => $horario['hora_inicio'],
+                            'hora_termino'      => $horario['hora_termino']
+                        );
+
+                        $result_horario = $conexion->query($phql, $values);
+                        $result_horario->setFetchMode(\Phalcon\Db\Enum::FETCH_ASSOC);
+                
+                        $flag_create    = false;
+                        if ($result_horario) {
+                            while ($data_horario = $result_horario->fetch()) {
+                                $flag_create    = true;
+                            }
+                        }
+
+                        if ($flag_create){
+                            //  SE VERIFICA SI EXISTE EL REGISTRO DE TBCITAS_PROGRAMADAS
+                            $phql   = "INSERT INTO tbcitas_programadas_servicios_horarios (id_cita_programada_servicio,dia,hora_inicio,hora_termino)
+                                        VALUES (:id_cita_programada_servicio,:dia,:hora_inicio,:hora_termino)";
+                            $result_insert_horario  = $conexion->query($phql, array(
+                                'id_cita_programada_servicio'   => $id_cita_programada_servicio,
+                                'dia'                           => $horario['dia'],
+                                'hora_inicio'                   => $horario['hora_inicio'],
+                                'hora_termino'                  => $horario['hora_termino']
+                            ));
+                        }
+                    }catch(\Exception $err){
+                        throw new \Exception($err->getMessage());
+                    }
+                    
+                }
+            }
+
+            $conexion->commit();
+    
+            // RESPUESTA JSON
+            $response = new Response();
+            $response->setJsonContent(array('MSG' => 'OK'));
+            $response->setStatusCode(200, 'OK');
+            return $response;
+            
+        } catch (\Exception $e) {
+            $conexion->rollback();
+            
+            return (new Response())->setJsonContent([
+                'status'  => 'error',
+                'message' => $e->getMessage()
+            ])->setStatusCode(400, 'Bad Request');
+        }
+    });
+
+    // Ruta principal para obtener todos los registros
+    $app->get('/ctpacientes/get_program_date', function () use ($app,$db,$request) {
+
+        try{
+
+            $id_paciente    = $request->getQuery('id_paciente') ?? null;
+            $id_locacion    = $request->getQuery('id_locacion') ?? null;
+
+            if (empty($id_paciente) && empty($id_locacion)){
+                throw new Exception('Parámetros vacíos');
+            }
+        
+            // Definir el query SQL
+            $phql   = " SELECT
+                            a.*,
+                            b.id as id_cita_programada_servicio,
+                            b.id_servicio,
+                            b.id_profesional,
+                            c.dia,
+                            c.hora_inicio,
+                            c.hora_termino  
+                        FROM tbcitas_programadas a 
+                        LEFT JOIN tbcitas_programadas_servicios b ON a.id = b.id_cita_programada
+                        LEFT JOIN tbcitas_programadas_servicios_horarios c ON b.id = c.id_cita_programada_servicio
+                        WHERE 1 = 1 ";
+            $values = array();
+    
+            if (is_numeric($id_paciente)){
+                $phql                   .= " AND a.id_paciente = :id_paciente ";
+                $values['id_paciente']  = $id_paciente;
+            }
+
+            if (is_numeric($id_locacion)){
+                $phql                   .= " AND a.id_locacion = :id_locacion ";
+                $values['id_locacion']  = $id_locacion;
+            }
+    
+            // Ejecutar el query y obtener el resultado
+            $result = $db->query($phql,$values);
+            $result->setFetchMode(\Phalcon\Db\Enum::FETCH_ASSOC);
+    
+            // Recorrer los resultados
+            $row    = [];
+            while ($data = $result->fetch()) {
+                $row[$data['id_cita_programada_servicio']]['id_servicio']       = $data['id_servicio'];
+                $row[$data['id_cita_programada_servicio']]['id_profesional']    = $data['id_profesional'];
+                $row[$data['id_cita_programada_servicio']]['horarios'][]        = array(
+                    'dia'           => $data['dia'],
+                    'hora_inicio'   => $data['hora_inicio'],
+                    'hora_termino'  => $data['hora_termino'],
+                );
+            }
+    
+            // Devolver los datos en formato JSON
+            $response = new Response();
+            $response->setJsonContent($row);
+            $response->setStatusCode(200, 'OK');
+            return $response;
+        }catch (\Exception $e){
+            // Devolver los datos en formato JSON
+            $response = new Response();
+            $response->setJsonContent($e->getMessage());
+            $response->setStatusCode(400, 'not found');
+            return $response;
+        }
+        
     });
 };
---
+++ sp_functions/fn_validar_citas_programadas.sql
@@ -0,0 +1,41 @@
+CREATE OR REPLACE FUNCTION fn_validar_citas_programadas(p_id_profesional INT, p_id_paciente INT, p_dia INT,p_label_dia VARCHAR, p_hora_inicio VARCHAR, p_hora_termino VARCHAR)
+RETURNS BOOLEAN AS $$
+DECLARE
+    -- Declaración de variables locales (si es necesario)
+    v_flag_insert       BOOLEAN;
+    v_citas             RECORD;
+    v_hora_inicio_time  TIME;
+    v_hora_termino_time TIME;
+BEGIN
+
+    v_flag_insert   := TRUE;
+    --  INICIACION DE VARIABLES PARA CONVERTIRLAS EN TIME '09:30:00'
+    v_hora_inicio_time  := p_hora_inicio::TIME;
+    v_hora_termino_time := p_hora_termino::TIME;
+    
+    --  SE BUSCAN TODAS LAS CITAS DEL PROFESIONAL, INDEPENDIENTEMENTE DE LA INSTITUCION
+    FOR v_citas IN 
+        SELECT 
+            c.*,
+            (e.primer_apellido|| ' ' || COALESCE(e.segundo_apellido,'') || ' '|| e.nombre) as nombre_paciente
+        FROM tbcitas_programadas a 
+        LEFT JOIN tbcitas_programadas_servicios b ON a.id = b.id_cita_programada
+        LEFT JOIN tbcitas_programadas_servicios_horarios c ON b.id = c.id_cita_programada_servicio
+        LEFT JOIN ctpacientes e ON a.id_paciente = e.id
+        WHERE (a.id_paciente = p_id_paciente OR b.id_profesional = p_id_profesional) AND c.dia = p_dia
+    LOOP 
+        --  SE VERIFICA QUE LA HORA INICIO NO SE EMPALME
+        IF v_hora_inicio_time >= v_citas.hora_inicio::TIME AND v_hora_inicio_time < v_citas.hora_termino::TIME THEN
+            RAISE EXCEPTION '$$El paciente (%) cuenta con un horario asignado que genera conflictos:  Día (%) de (%) a (%)##',v_citas.nombre_paciente,p_label_dia,p_hora_inicio,p_hora_termino;
+        END IF; 
+
+        --  SE VERIFICA QUE LA HORA DE TERMINO NO SE EMPALME
+        IF v_hora_termino_time > v_citas.hora_inicio::TIME AND v_hora_termino_time <= v_citas.hora_termino::TIME THEN
+            RAISE EXCEPTION 'El paciente (%) cuenta con un horario asignado que genera conflictos:  Día (%) de (%) a (%)',v_citas.nombre_paciente,p_label_dia,p_hora_inicio,p_hora_termino;
+        END IF;
+
+    END LOOP;
+
+    RETURN v_flag_insert;
+END;
+$$ LANGUAGE plpgsql;
