3ef526cf53b15de6f3c0d1b89603a9029f74f2f5
Hugo Munoz <proyectos.munoz17@gmail.com>
2025-06-10 14:25:18 -0700

    Modificacion para conectarse de forma local o la nube

--- public/index.php
+++ public/index.php
@@ -17,31 +17,33 @@ $config = require '../config.php';
 
 // Crear el contenedor de inyección de dependencias
 $di = new FactoryDefault();
-// $di->set('db', function () use ($config) {
-//     return new PdoPostgres([
-//         'host'      => $config['database']['host'],
-//         'username'  => $config['database']['username'],
-//         'password'  => $config['database']['password'],
-//         'dbname'    => $config['database']['dbname'],
-//         'port'      => $config['database']['port']
-//     ]);
-// });
 
-$di->setShared('db', function () use ($config) {
-    return new PdoPostgres([
-        'host'      => $config['database']['host'],
-        'username'  => $config['database']['username'],
-        'password'  => $config['database']['password'],
-        'dbname'    => $config['database']['dbname'],
-        'port'      => $config['database']['port'],
-        'sslmode'   => 'require',
-        'sslrootcert' => $config['database']['sslrootcert'],
-        'options'   => [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        ]
-    ]);
-});
-
+if ($config['database']['local']){
+    $di->set('db', function () use ($config) {
+        return new PdoPostgres([
+            'host'      => $config['database']['host'],
+            'username'  => $config['database']['username'],
+            'password'  => $config['database']['password'],
+            'dbname'    => $config['database']['dbname'],
+            'port'      => $config['database']['port']
+        ]);
+    });
+}else {
+    $di->setShared('db', function () use ($config) {
+        return new PdoPostgres([
+            'host'      => $config['database']['host'],
+            'username'  => $config['database']['username'],
+            'password'  => $config['database']['password'],
+            'dbname'    => $config['database']['dbname'],
+            'port'      => $config['database']['port'],
+            'sslmode'   => 'require',
+            'sslrootcert' => $config['database']['sslrootcert'],
+            'options'   => [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]
+        ]);
+    });
+}
 
 // 🔹 Registrar el servicio 'request' en el DI (Corrección del error)
 $di->setShared('request', function () {
