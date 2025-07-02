08288a8becf71b05b235263275b6b30e78c30fe5
Hugo Munoz <proyectos.munoz17@gmail.com>
2025-07-01 20:03:43 -0700

    Correcciones en funciones

--- sp_functions/fn_validar_citas_programadas.sql
+++ sp_functions/fn_validar_citas_programadas.sql
@@ -35,9 +35,11 @@ BEGIN
         END IF;
 
         --  SE VERIFICA QUE LA HORA DE INICIO Y TERMINO ENGLOBEN A LA HORA INDICADA
-        IF v_hora_inicio_time > v_citas.hora_inicio::TIME AND v_hora_termino_time > v_citas.hora_termino::TIME THEN
-            RAISE EXCEPTION 'El paciente (%) cuenta con un horario asignado que genera conflictos:  Día (%) de (%) a (%)...',v_citas.nombre_paciente,p_fecha_cita,p_hora_inicio,p_hora_termino;
+        IF v_hora_inicio_time < v_citas.hora_termino::TIME AND v_hora_termino_time > v_citas.hora_inicio::TIME THEN
+        RAISE EXCEPTION 'El paciente (%) cuenta con un horario asignado que genera conflictos: Día (%) de (%) a (%)...',
+            v_citas.nombre_paciente, p_label_dia, p_hora_inicio, p_hora_termino;
         END IF;
+
 
     END LOOP;
 
