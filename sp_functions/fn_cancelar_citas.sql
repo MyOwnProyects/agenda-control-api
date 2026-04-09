5d31f63d5f231d02f23d82c2dd0183de111a61e9
Hugo Munoz <proyectos.munoz17@gmail.com>
2026-04-08 18:42:00 -0700

    Funcion trigger para cancelar movtos de citas que se cancelaron

---
+++ sp_functions/fn_cancelar_citas.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE FUNCTION fn_cancelar_citas()
+RETURNS TRIGGER AS $$
+BEGIN
+
+    IF (NEW.activa <> 0) THEN 
+        RETURN NULL;
+    END IF;
+
+    --  ESTA FUNCION REALIZA LA CANCELACION DE MOVTOS
+    --  DESPUES DE HABER CANCELADO UNA CITA
+    UPDATE tbabonos_movimientos SET 
+        estatus = 0,
+        id_usuario_cancelacion = NEW.id_usuario_cancelacion,
+        fecha_cancelacion = NEW.fecha_cancelacion
+    WHERE id_agenda_cita = NEW.id AND estatus <> 0;
+
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+/* CREATE TRIGGER trg_fn_cancelar_citas
+AFTER UPDATE ON tbagenda_citas
+FOR EACH ROW
+WHEN (NEW.estatus = 0)  -- Solo cuando se cancela
+EXECUTE FUNCTION fn_cancelar_citas(); */
