f787954c66f0fb17a49816eb58405c8c62603959
Hugo Munoz <proyectos.munoz17@gmail.com>
2026-05-31 21:50:25 -0700

    Rutas para asignacion de becas

--- Rutas/Becas.php
+++ Rutas/Becas.php
@@ -304,4 +304,122 @@ return function (Micro $app,$di) {
             ])->setStatusCode(400, 'Bad Request');
         }
     });
+
+    //  ASIGNACION DE BECAS
+    // Ruta principal para obtener todos los usuarios
+    $app->get('/paciente_becas/count', function () use ($app,$db,$request) {
+        try{
+
+            $id             = $request->getQuery('id');
+            $id_paciente    = $request->getQuery('clave');
+        
+            // Definir el query SQL
+            $phql   = "SELECT 
+                            COUNT(1) as num_registros
+                        FROM tbpaciente_becas a 
+                        WHERE 1 = 1 ";
+            $values = array();
+    
+            if (is_numeric($id)){
+                $phql           .= " AND a.id = :id";
+                $values['id']   = $id;
+            }
+
+            if (is_numeric($id_paciente)){
+                $phql                   .= " AND a.id_paciente = :id_paciente";
+                $values['id_paciente']  = $id_paciente;
+            }
+    
+            // Ejecutar el query y obtener el resultado
+            $result = $db->query($phql,$values);
+            $result->setFetchMode(\Phalcon\Db\Enum::FETCH_ASSOC);
+    
+            // Recorrer los resultados
+            $num_registros  = 0;
+            while ($row = $result->fetch()) {
+                $num_registros  = $row['num_registros'];
+            }
+    
+            // Devolver los datos en formato JSON
+            $response = new Response();
+            $response->setJsonContent($num_registros);
+            $response->setStatusCode(200, 'OK');
+            return $response;
+        }catch (\Exception $e){
+            // Devolver los datos en formato JSON
+            $response = new Response();
+            $response->setContent($e->getMessage());
+            $response->setStatusCode(400, 'Created');
+            return $response;
+        }
+        
+    });
+
+    // Ruta principal para obtener todos los registros
+    $app->get('/paciente_becas/show', function () use ($app,$db,$request) {
+        try{
+
+            $id             = $request->getQuery('id');
+            $id_paciente    = $request->getQuery('clave');
+        
+            // Definir el query SQL
+            $phql   = " SELECT 
+                            a.*,
+                            (b.clave|| ' - ' || b.nombre) as beca,
+                            COALESCE(c.monto_usado,0) as monto_usado
+                        FROM tbpaciente_becas a 
+                        LEFT JOIN ctbecas b ON a.id_beca = b.id
+                        LEFT JOIN LATERAL (
+                            SELECT SUM(t1.monto) AS monto_usado 
+                            FROM tbabonos_movimientos t1
+                            LEFT JOIN tbabonos t2 ON t1.id_abono = t2.id
+                            WHERE a.id = t2.id_paciente_beca 
+                            AND (t1.estatus = 1 OR (t1.estatus = 0 AND t1.tipo_cancelacion = 2))
+                        ) c ON TRUE
+                        WHERE 1 = 1 ";
+            $values = array();
+    
+            if (is_numeric($id)){
+                $phql           .= " AND a.id = :id";
+                $values['id']   = $id;
+            }
+
+            if (is_numeric($id_paciente)){
+                $phql                   .= "AND a.id_paciente = :id_paciente";
+                $values['id_paciente']  = $id_paciente;
+            }
+            
+            $phql   .= ' ORDER BY a.fecha_captura DESC ';
+
+            if ($request->hasQuery('offset')){
+                $phql   .= " LIMIT ".$request->getQuery('length').' OFFSET '.$request->getQuery('offset');
+            }
+    
+            // Ejecutar el query y obtener el resultado
+            $result = $db->query($phql,$values);
+            $result->setFetchMode(\Phalcon\Db\Enum::FETCH_ASSOC);
+    
+            // Recorrer los resultados
+            $data = [];
+            while ($row = $result->fetch()) {
+                $row['label_estatus']           = $row['estatus'] == 1 ? 'ACTIVA' : 'INACTIVA';
+                $row['label_tipo_beca']         = $row['tipo_beca'] == 1 ? 'IMPORTE' : 'PORCENTAJE';
+                $row_['label_fecha_captura']    = FuncionesGlobales::formatearFecha($row['label_fecha_captura'],'d/m/Y H:i');
+                $data[]                         = $row;
+            }
+    
+            // Devolver los datos en formato JSON
+            $response = new Response();
+            $response->setJsonContent($data);
+            $response->setStatusCode(200, 'OK');
+            return $response;
+        }catch (\Exception $e){
+            // Devolver los datos en formato JSON
+            $response = new Response();
+            $response->setJsonContent($e->getMessage());
+            $response->setStatusCode(400, 'not found');
+            return $response;
+        }
+        
+    });
 };
